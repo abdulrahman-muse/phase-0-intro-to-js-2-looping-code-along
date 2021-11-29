8d00e04d52624be31b5fee00d99489cb0bae698d
abdulrahman muse <abdulrahmanmuse1@gmail.com>
2021-11-29 03:32:15 +0100

    commit tree 9398039830df08247de8281ab4b3bbf02fd43185

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(namesArray, event){
+let newArray = []
+  for(let i = 0; i < namesArray.length; i++){
+    newArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+  }
+  return newArray
+}
+
+function countDown(startingNumber){
+  while(startingNumber > 0){
+  console.log(startingNumber)
+  startingNumber -= 1;
+  }
+  console.log(startingNumber)
+}
